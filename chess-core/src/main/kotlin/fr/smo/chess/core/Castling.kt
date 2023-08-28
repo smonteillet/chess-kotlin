55a45b52a6eb75b86a4c80f82fc5fc8fc65e596b
Sylvain MONTEILLET <sylvain.monteillet@gmail.com>
2023-08-28 19:06:25 +0200

    [core] refactor castling

--- chess-core/src/main/kotlin/fr/smo/chess/core/Castling.kt
+++ chess-core/src/main/kotlin/fr/smo/chess/core/Castling.kt
@@ -1,5 +1,6 @@
 package fr.smo.chess.core
 
+import fr.smo.chess.core.Color.BLACK
 import fr.smo.chess.core.Color.WHITE
 
 data class Castling(
@@ -22,61 +23,37 @@ data class Castling(
 
     private fun isWhiteKingSideCastlingStillPossible(move: Move): Boolean {
         return isWhiteKingCastlePossible &&
-                (
-                        (
-                                move.piece.color == Color.BLACK &&
-                                        ((move.capturedPiece != null && move.destination != Square.H1) || move.capturedPiece == null)
-                                ) ||
-                                (
-                                        move.piece.color == WHITE &&
-                                                move.piece != Piece.WHITE_KING &&
-                                                (move.piece != Piece.WHITE_ROOK || move.from != Square.H1)
-                                        )
-                        )
+                if (move.piece.color == BLACK) {
+                    ((move.capturedPiece != null && move.destination != Square.H1) || move.capturedPiece == null)
+                } else {
+                    move.piece != Piece.WHITE_KING && (move.piece != Piece.WHITE_ROOK || move.from != Square.H1)
+                }
     }
 
     private fun isWhiteQueenSideCastlingStillPossible(move: Move): Boolean {
         return isWhiteQueenCastlePossible &&
-                (
-                        (
-                                move.piece.color == Color.BLACK &&
-                                        ((move.capturedPiece != null && move.destination != Square.A1) || move.capturedPiece == null)
-                                ) ||
-                                (
-                                        move.piece.color == WHITE &&
-                                                move.piece != Piece.WHITE_KING &&
-                                                (move.piece != Piece.WHITE_ROOK || move.from != Square.A1)
-                                        )
-                        )
+                if (move.piece.color == BLACK) {
+                    ((move.capturedPiece != null && move.destination != Square.A1) || move.capturedPiece == null)
+                } else {
+                    move.piece != Piece.WHITE_KING && (move.piece != Piece.WHITE_ROOK || move.from != Square.A1)
+                }
     }
 
     private fun isBlackKingSideCastlingStillPossible(move: Move): Boolean {
         return isBlackKingCastlePossible &&
-                (
-                        (
-                                move.piece.color == WHITE &&
-                                        ((move.capturedPiece != null && move.destination != Square.H8) || move.capturedPiece == null)
-                                ) ||
-                                (
-                                        move.piece.color == Color.BLACK &&
-                                                move.piece != Piece.BLACK_KING &&
-                                                (move.piece != Piece.BLACK_ROOK || move.from != Square.H8)
-                                        )
-                        )
+                if (move.piece.color == WHITE) {
+                    ((move.capturedPiece != null && move.destination != Square.H8) || move.capturedPiece == null)
+                } else {
+                    move.piece != Piece.BLACK_KING && (move.piece != Piece.BLACK_ROOK || move.from != Square.H8)
+                }
     }
 
     private fun isBlackQueenSideCastlingStillPossible(move: Move): Boolean {
         return isBlackQueenCastlePossible &&
-                (
-                        (
-                                move.piece.color == WHITE &&
-                                        ((move.capturedPiece != null && move.destination != Square.A8) || move.capturedPiece == null)
-                                ) ||
-                                (
-                                        move.piece.color == Color.BLACK &&
-                                                move.piece != Piece.BLACK_KING &&
-                                                (move.piece != Piece.BLACK_ROOK || move.from != Square.A8)
-                                        )
-                        )
+                if (move.piece.color == WHITE) {
+                    ((move.capturedPiece != null && move.destination != Square.A8) || move.capturedPiece == null)
+                } else {
+                    move.piece != Piece.BLACK_KING && (move.piece != Piece.BLACK_ROOK || move.from != Square.A8)
+                }
     }
 }
